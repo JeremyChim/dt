79cb23ba9ec1c4e5addc9a1a86fdf6aad14944b6
JeremyChim <jer888chim@outlook.com>
2025-09-24 14:30:15 +0800

    更新

---
+++ 3. gi.py
@@ -0,0 +1,17 @@
+reset = True # 是否重置
+
+# 读取文件
+if reset is False:
+    with open("gi/gameinfo_branchspecific.gi", encoding='utf-8') as f:  # 旧的gameinfo_branchspecific.gi
+        lines = f.readlines()
+else:
+    with open("gi/gameinfo_branchspecific2.gi", encoding='utf-8') as f:  # 新的gameinfo_branchspecific.gi
+        lines = f.readlines()
+
+new_lines = []
+for i, line in enumerate(lines, 1):
+    new_lines.append(line)
+
+# 写入文件
+with open("gameinfo_branchspecific.gi", "w", encoding='utf-8') as f:
+    f.writelines(new_lines)
