9263d4d3a4d713fe0a16c6f501e60eb661065f99
JeremyChim <jer888chim@outlook.com>
2025-09-24 14:12:00 +0800

    更新

--- 0. run.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import shutil
-import time
-
-subprocess.run(r"C:\Users\Jeremy\Desktop\dt\vpk\vpk.bat", shell=True)
-time.sleep(0.1)
-shutil.move(r"C:\Users\Jeremy\Desktop\dt\vpk\pak01_dir.vpk", r"C:\Users\Jeremy\Desktop\dt\pak01_dir.vpk")
--- 6. open_pak01.py
+++
@@ -1,4 +0,0 @@
-import os
-
-path = r"E:\GAME\steamapps\common\dota 2 beta\game\dota\pak01_dir.vpk"
-os.startfile(path)
